da44459bca355e8b330a9f0ac70dacc860b6007c
Pornkamol Luthra <suhavni@gmail.com>
2021-05-30 23:53:58 +0700

    hw 2-1

---
+++ src/main/java/io/muic/ooc/fab/Fox.java
@@ -0,0 +1,111 @@
+package io.muic.ooc.fab;
+
+import java.util.List;
+import java.util.Random;
+
+public class Fox extends Animal {
+    // Characteristics shared by all foxes (class variables).
+
+    // The age at which a fox can start to breed.
+    private static final int BREEDING_AGE = 15;
+    // The age to which a fox can live.
+    private static final int MAX_AGE = 150;
+    // The likelihood of a fox breeding.
+    private static final double BREEDING_PROBABILITY = 0.08;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 2;
+    // The food value of a single rabbit. In effect, this is the
+    // number of steps a fox can go before it has to eat again.
+    private static final int RABBIT_FOOD_VALUE = 9;
+    // Random generator
+    private static final Random RANDOM = new Random();
+
+    // The fox's food level, which is increased by eating rabbits.
+    private int foodLevel;
+
+    /**
+     * Create a fox. A fox can be created as a new born (age zero and not
+     * hungry) or with a random age and food level.
+     *
+     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Fox(boolean randomAge, Field field, Location location) {
+        super(randomAge, field, location);
+        foodLevel = RANDOM.nextInt(RABBIT_FOOD_VALUE);
+    }
+
+    @Override
+    public Location generateNewLocation() {
+        Location newLocation = findFood();
+        if (newLocation == null) {
+            // No food found - try to move to a free location.
+            newLocation = field.freeAdjacentLocation(getLocation());
+        }
+        return newLocation;
+    }
+
+    @Override
+    public void nextStep(List<Animal> animals) {
+        incrementHunger();
+        super.nextStep(animals);
+    }
+
+    /**
+     * Make this fox more hungry. This could result in the fox's death.
+     */
+    private void incrementHunger() {
+        foodLevel--;
+        if (foodLevel <= 0) {
+            setDead();
+        }
+    }
+
+    /**
+     * Look for rabbits adjacent to the current location. Only the first live
+     * rabbit is eaten.
+     *
+     * @return Where food was found, or null if it wasn't.
+     */
+    private Location findFood() {
+        List<Location> adjacent = field.adjacentLocations(location);
+        for (Location where : adjacent) {
+            Object animal = field.getObjectAt(where);
+            if (animal instanceof Rabbit) {
+                Rabbit rabbit = (Rabbit) animal;
+                if (rabbit.isAlive()) {
+                    rabbit.setDead();
+                    foodLevel = RABBIT_FOOD_VALUE;
+                    return where;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected double getBreedingProbability() {
+        return BREEDING_PROBABILITY;
+    }
+
+    @Override
+    protected int getMaxLitterSize() {
+        return MAX_LITTER_SIZE;
+    }
+
+    @Override
+    protected int getBreedingAge() {
+        return BREEDING_AGE;
+    }
+
+    @Override
+    protected Animal createYoung(boolean randomAge, Field field, Location location) {
+        return new Fox(randomAge, field, location);
+    }
+}
---
+++ src/main/java/io/muic/ooc/fab/Rabbit.java
@@ -0,0 +1,61 @@
+package io.muic.ooc.fab;
+
+import java.util.List;
+import java.util.Random;
+
+public class Rabbit extends Animal {
+    // Characteristics shared by all rabbits (class variables).
+
+    // The age at which a rabbit can start to breed.
+    private static final int BREEDING_AGE = 5;
+    // The age to which a rabbit can live.
+    private static final int MAX_AGE = 40;
+    // The likelihood of a rabbit breeding.
+    private static final double BREEDING_PROBABILITY = 0.12;
+    // The maximum number of births.
+    private static final int MAX_LITTER_SIZE = 4;
+    // A shared random number generator to control breeding.
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Create a new rabbit. A rabbit may be created with age zero (a new born)
+     * or with a random age.
+     *
+     * @param randomAge If true, the rabbit will have a random age.
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Rabbit(boolean randomAge, Field field, Location location) {
+        super(randomAge, field, location);
+    }
+
+    @Override
+    protected Location generateNewLocation() {
+        return field.freeAdjacentLocation(getLocation());
+    }
+
+    @Override
+    protected double getBreedingProbability() {
+        return BREEDING_PROBABILITY;
+    }
+
+    @Override
+    protected int getMaxLitterSize() {
+        return MAX_LITTER_SIZE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected int getBreedingAge() {
+        return BREEDING_AGE;
+    }
+
+    @Override
+    protected Animal createYoung(boolean randomAge, Field field, Location location) {
+        return new Rabbit(randomAge, field, location);
+    }
+}
