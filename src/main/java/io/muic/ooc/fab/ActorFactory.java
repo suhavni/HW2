a9135c2e7a406934da27d5fcb1ea6fb2f14b8e91
Pornkamol Luthra <suhavni@gmail.com>
2021-06-01 13:20:38 +0700

    hw 2-2 ver 10

--- src/main/java/io/muic/ooc/fab/ActorFactory.java
+++ src/main/java/io/muic/ooc/fab/ActorFactory.java
@@ -4,17 +4,13 @@ import java.lang.reflect.InvocationTargetException;
 
 public class ActorFactory {
     public static Actor createActor(ActorType actorType, boolean randomAge, Field field, Location location) {
-            try {
-                return actorType.getActorClass().getConstructor(boolean.class, Field.class, Location.class).newInstance(randomAge, field, location);
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-            return null;
+        try {
+            return actorType.getActorClass()
+                    .getConstructor(boolean.class, Field.class, Location.class)
+                    .newInstance(randomAge, field, location);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
